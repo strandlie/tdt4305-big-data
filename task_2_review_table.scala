978c7948d8a42192b618a706a6909e4b7ca49172
Håkon Strandlie <haakstr@stud.ntnu.no>
2020-02-29 15:37:41 +0100

    Finished task2

--- task_2_review_table.scala
+++ task_2_review_table.scala
@@ -2,16 +2,38 @@ import org.apache.spark
 
 object Task2 {
 
-    def main () {
+    def setup () = {
         val reviews_file = sc.textFile("../Data/yelp_top_reviewers_with_reviews.csv")
-        val reviews = reviews_file.map(_.split("\t"))
-        var mapByKey = (review: Array[String]) => {
-            val id = review(1)
-            val rest = review.slice(0, 1) ++ review.slice(2, -1)
-            (id, rest)
-        }
-        val reviews_by_user_id = reviews.map(mapByKey).filter(_._1 != "user_id")
-        reviews_by_user_id.take(5).foreach(println)
+        reviews_file.map(_.split("\t"))
+    }
+
+    def a () {
+        val reviews = setup()
+        val ids = reviews.map (review => review(1))
+            .filter(id => id != """"user_id"""")
+        val num_distinct_users = ids.distinct.count
+        println(num_distinct_users)
+    }
+
+    def b () {
+        val reviews = setup()
+        val review_texts = reviews.map (review => review(3))
+            .filter(text => { 
+                try {
+                    new String(java.util.Base64.getDecoder.decode(text))
+                    text != """"review_text"""" 
+                } catch {
+                    case e: IllegalArgumentException => {
+                        false
+                    }
+                }
+            })
+            .map(base64_text => { new String(java.util.Base64.getDecoder.decode(base64_text))})
+        
+        val character_counts = review_texts.map(text => text.length)
+        val sum = character_counts.reduce((a, b) => a + b)
+        val average = sum / character_counts.count
+        println(average)
 
     }
 }
