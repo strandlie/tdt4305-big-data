9b79e3f76cb90b498b1d1b4e9f6d941650e43477
Håkon Strandlie <haakstr@stud.ntnu.no>
2020-03-06 12:53:02 +0100

    Updated to sbt-setup, and finished task 4, 5 and most of 6

---
+++ src/main/scala/task_5.scala
@@ -0,0 +1,49 @@
+package yelp
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Encoders
+
+
+case class ReviewTableSchema(review_id: String, user_id: String, business_id: String, review_text: String, review_date: String) 
+case class FriendshipTableSchema(src_user_id: String, dst_user_id: String)
+case class BusinessTableSchema(business_id: String, name: String, address: String, city: String, state: String, postal_code: String, latitude: Float, longitude: Float, stars: Float, review_count: Int, categories: String)
+
+object Task5 {
+
+    def a (spark : SparkSession) = {
+        val friendshipTableSchema = Encoders.product[FriendshipTableSchema].schema
+        val friendshipTableDf = spark.read.format("csv")
+                    .option("header", "true")
+                    .option("delimiter", ",")
+                    .schema(friendshipTableSchema)
+                    .load("../Data/yelp_top_users_friendship_graph.csv")
+
+        val reviewTableSchema = Encoders.product[ReviewTableSchema].schema
+        val reviewTableDf = spark.read.format("csv")
+                    .option("header", "true")
+                    .option("delimiter", "\t")
+                    .schema(reviewTableSchema)
+                    .load("../Data/yelp_top_reviewers_with_reviews.csv")
+
+        val businessTableSchema = Encoders.product[BusinessTableSchema].schema
+        val businessTableDf = spark.read.format("csv")
+                    .option("header", "true")
+                    .option("delimiter", "\t")
+                    .schema(businessTableSchema)
+                    .load("../Data/yelp_businesses.csv")
+
+        (friendshipTableDf, reviewTableDf, businessTableDf)
+    }
+
+    def main(args: Array[String]) {
+        val spark = SparkSession.builder
+                        .appName("Task 5)")
+                        .master("local")
+                        .getOrCreate()
+
+        a(spark)
+        spark.stop()
+
+
+    }
+}
